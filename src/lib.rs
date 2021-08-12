b14b90e07700d74f5e4e8c53b5a7ca313e1d8b0f
cht <3237126351@qq.com>
2021-08-12 23:07:03 +0800

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use gtk::prelude::*;
 use std::f64::consts::PI;
 /// let wndow = gtk::ApplicationWindow::new(application);
 /// add_corner(window,0.02);
-pub fn add_corner(window: &gtk::ApplicationWindow,radius: f64) {
+pub fn add_corner(window: &gtk::ApplicationWindow, radius: f64) {
     window.connect_configure_event(move |window, _| {
         //清除绘制
         gdk::Window::invalidate_rect(&window.window().unwrap(), Some(&window.allocation()), false);
@@ -28,26 +28,26 @@ pub fn add_corner(window: &gtk::ApplicationWindow,radius: f64) {
         cr.set_line_width(0.1);
         cr.set_source_rgb(0.0, 0.0, 0.0);
         cr.move_to(radius, 0.0);
-        cr.line_to(1.0-radius, 0.0);
+        cr.line_to(1.0 - radius, 0.0);
         //cr.stroke().expect("Invalid cairo surface state");
         //cr.arc(0.1, 0.1, 0.1, PI, PI*3.0/2.0);
         cr.move_to(1.0, radius);
-        cr.line_to(1.0, 1.0-radius);
+        cr.line_to(1.0, 1.0 - radius);
         //cr.stroke().expect("Invalid cairo surface state");
         //cr.arc(0.1, 0.9, 0.1, PI/2.0, PI);
-        cr.move_to(1.0-radius, 1.0);
+        cr.move_to(1.0 - radius, 1.0);
         cr.line_to(radius, 1.0);
         //cr.stroke().expect("Invalid cairo surface state");
         //cr.arc(0.9, 0.9, 0.1, 0.0, PI/2.0);
-        cr.move_to(0.0, 1.0-radius);
+        cr.move_to(0.0, 1.0 - radius);
         cr.line_to(0.0, radius);
         //cr.stroke().expect("Invalid cairo surface state");
 
         cr.arc(radius, radius, radius, PI, PI * 3.0 / 2.0);
-        cr.arc(1.0-radius, radius, radius, -PI / 2.0, 0.0);
+        cr.arc(1.0 - radius, radius, radius, -PI / 2.0, 0.0);
 
-        cr.arc(1.0-radius, 1.0-radius, radius, 0.0, PI / 2.0);
-        cr.arc(radius, 1.0-radius, radius, PI / 2.0, PI);
+        cr.arc(1.0 - radius, 1.0 - radius, radius, 0.0, PI / 2.0);
+        cr.arc(radius, 1.0 - radius, radius, PI / 2.0, PI);
 
         //cr.stroke_preserve().unwrap();
         cr.fill().expect("sss");
